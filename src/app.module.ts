b5d6fdfe8ea3b9cb846019d03c7381077e6fffd2
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-12 20:41:43 +0200

    Add basic controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RestaurantsController } from './restaurants/restaurants.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, RestaurantsController],
   providers: [AppService],
 })
 export class AppModule {}
