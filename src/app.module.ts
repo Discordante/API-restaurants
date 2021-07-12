4deafccf2bd93a060c691ae34008cdd23fee7cbb
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-12 21:14:39 +0200

    Add basic services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsController } from './restaurants/restaurants.controller';
+import { RestaurantsService } from './restaurants/restaurants.service';
 
 @Module({
   imports: [],
   controllers: [AppController, RestaurantsController],
-  providers: [AppService],
+  providers: [AppService, RestaurantsService],
 })
 export class AppModule {}
--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -7,31 +7,34 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
 
 @Controller('restaurants')
 export class RestaurantsController {
+  constructor(private readonly restaurantsService: RestaurantsService) {}
+
   @Get()
   findAll() {
-    return `This action returns all restaurants`;
+    return this.restaurantsService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} restaurant`;
+    return this.restaurantsService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.restaurantsService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} restaurant`;
+    return this.restaurantsService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} restaurant`;
+    return this.restaurantsService.remove(id);
   }
 }
