cc2939b8e3ba8a5d751e7c78967b5f0e096983c6
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-12 21:59:38 +0200

    add new module to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RestaurantsController } from './restaurants/restaurants.controller';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [RestaurantsModule],
-  controllers: [AppController, RestaurantsController],
-  providers: [AppService, RestaurantsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
