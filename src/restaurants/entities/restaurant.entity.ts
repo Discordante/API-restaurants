405ddbe7637f0e81efa650a08d1d370b01cdb231
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-13 09:54:51 +0200

    Add Restaurant schema

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,15 @@
-export class Restaurant {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Restaurant extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   priceAvg: number;
+
+  @Prop()
   category: string;
 }
+
+export const RestaurantSchema = SchemaFactory.createForClass(Restaurant);
