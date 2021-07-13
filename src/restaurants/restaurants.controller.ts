f970656c83e467d40fc27c7ca427a099b282f3d4
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-13 10:19:02 +0200

    crud first approach

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -23,8 +23,8 @@ export class RestaurantsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.restaurantsService.findOne('' + id);
+  findOne(@Param('id') id: string) {
+    return this.restaurantsService.findOne(id);
   }
 
   @Post()
