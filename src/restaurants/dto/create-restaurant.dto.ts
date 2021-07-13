121fcb1bb8701c380e98ec3db269ba0cd0ea8b28
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-13 09:38:15 +0200

    Add validation dto pipes

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,5 +1,12 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreateRestaurantDto {
+  @IsString()
   readonly name: string;
+
+  @IsNumber()
   readonly priceAvg: number;
+
+  @IsString()
   readonly category: string;
 }
