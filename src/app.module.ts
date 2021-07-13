fab25fa329b9db6806b822194dbd8a67cbef00d9
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-07-13 09:47:46 +0200

    Add connection to remote db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [RestaurantsModule],
+  imports: [
+    RestaurantsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://farsante:Mofletescontraelmundo95.@cluster0.yvoz8.mongodb.net/API-restaurants',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
